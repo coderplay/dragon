4ee5f58369a815c62cdaf8a0de5efe808e3aabe4
Min Zhou <coderplay@gmail.com>
2012-05-23 21:48:03 +0800

    Fixs a bug that would cause build failure. JobSubmitter cannot find symbol class DelegationTokenIdentifier

--- hadoop-dragon-core/src/main/java/org/apache/hadoop/realtime/DragonVertex.java
+++ hadoop-dragon-core/src/main/java/org/apache/hadoop/realtime/DragonVertex.java
@@ -18,7 +18,10 @@
 package org.apache.hadoop.realtime;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.realtime.event.EventProcessor;
 import org.apache.hadoop.realtime.event.EventProducer;
 
@@ -28,14 +31,39 @@ import org.apache.hadoop.realtime.event.EventProducer;
 public class DragonVertex implements Serializable {
 
   private static final long serialVersionUID = -8959502704094556166L;
+  private String label;
+  private int tasks;
+  private List<Path> files;
+  private List<Path> archives;
+  private String childOpts;
 
   DragonVertex() {
   };
 
   DragonVertex(Builder builder) {
-
+    label = builder.label;
+    files = builder.files;
+    archives = builder.archives;
+    childOpts = builder.childOpts;
+    tasks = (builder.tasks > 0) ? builder.tasks : 1;
   }
   
+  public String getLabel() {
+    return label;
+  }
+  
+  List<Path> getFiles() {
+    return files;
+  }
+  
+  List<Path> getArchives() {
+    return archives;
+  }
+  
+  String getChildOptins() {
+    return childOpts;
+  }
+
   @Override
   public int hashCode() {
     // TODO: implements it
@@ -44,36 +72,44 @@ public class DragonVertex implements Serializable {
 
   public static final class Builder {
     String label;
+    int tasks;
+    List<Path> jars;
+    List<Path> files;
+    List<Path> archives;
+    String childOpts;
 
-    public Builder(String label) {
+    public Builder(final String label) {
       this.label = label;
+      jars = new ArrayList<Path>();
+      files = new ArrayList<Path>();
+      archives = new ArrayList<Path>();
     }
 
-    public Builder jars(String[] jars) {
+    public Builder addArchieve(final Path archive){
+      archives.add(archive);
       return this;
     }
 
-    public Builder archieves(String[] archieves) {
+    public Builder files(final Path file) {
+      files.add(file);
       return this;
     }
 
-    public Builder files(String[] files) {
+    public Builder producer(final Class<? extends EventProducer> clazz) {
       return this;
     }
 
-    public Builder producer(Class<? extends EventProducer> clazz) {
+    public Builder processor(final Class<? extends EventProcessor> clazz) {
       return this;
     }
 
-    public Builder processor(Class<? extends EventProcessor> clazz) {
+    public Builder tasks(final int num) {
+      this.tasks = num;
       return this;
     }
 
-    public Builder tasks(int num) {
-      return this;
-    }
-
-    public Builder childOptions(String options) {
+    public Builder childOptions(final String options) {
+      childOpts = options;
       return this;
     }
 
